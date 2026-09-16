907f7c4cb7bf04372cf9dd5e928afe3af9ff361a
Saad Hamid <csaad8719@example.com>
2026-09-16 11:47:33 -0700

    Configure for local docker and GitHub Pages deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
+
 export default defineConfig({
   base: process.env.VITE_BASE_PATH || "/",
   plugins: [react()],
